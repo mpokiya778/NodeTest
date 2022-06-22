680c9f5d68bdd3800646a889afc82598f5e0b4d3
mpokiya778 <75971723+mpokiya778@users.noreply.github.com>
2022-06-22 10:27:07 +0530

    hosted

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var router = require('./route')
 
-var app = express();
-app.use('/api',router);
-app.listen(process.env.PORT,()=>{
-    console.log("Server Started");
+mongoose.connect("mongodb+srv://Monali:monali5401@cluster0.ydavz.mongodb.net/Movies?retryWrites=true&w=majority").then(()=>{
+    console.log("DB Connected");
+
+    var app = express();
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(express.json())
+    app.use('api',router)
+
+    app.listen(process.env.PORT,()=>{
+        console.log("server started")
+    })
+
+}).catch((err)=>{
+    console.log(err);
 })
+
